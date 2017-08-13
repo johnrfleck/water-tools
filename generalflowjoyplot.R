ed66a34c8bea7e189e7e1f0f4bddccb0d14fae95
johnrfleck <jfleck@inkstain.net>
2017-08-12 20:28:08 -0600

    comment the code

--- generalflowjoyplot.R
+++ generalflowjoyplot.R
@@ -1,23 +1,29 @@
-# general flow
-# tutorial here: https://owi.usgs.gov/R/dataRetrieval.html#1
+# John Fleck
+# University of New Mexico Water Resources Program
+# based on code by Lauren Steely, @MadreDeZanjas
+#   https://github.com/codeswitching/Reservoir-inflow-analysis/tree/master
 
-library(dataRetrieval)
-library(tidyverse)
-library(lubridate)
-library(cowplot)
-library(ggjoy)
+# Create joyplot of daily discharge for an arbitrary USGS stream gauge
+# uses USGS dataRetrieval package
+# tutorial for data retrieval package here: https://owi.usgs.gov/R/dataRetrieval.html#1
 
-#get gauge number
+library(dataRetrieval) # USGS data retrieval package
+library(tidyverse) # data tidying toolkit
+library(lubridate) # for date handling
+library(cowplot) # for prettying the pictures
+library(ggjoy) # create joyplots 
+               # https://cran.r-project.org/web/packages/ggjoy/vignettes/introduction.html
 
+# prompt user for gauge number
 siteNo <- readline(prompt="Enter a gauge number: ")
 
-# parameters for USGS API
-
+# get station name for later display purposes
 sta_meta <- readNWISsite(siteNo)
 
+# reality check
 print(sta_meta$station_nm)
 
-# reasonable start date
+# reasonable start date - this is the date of the first gauge record, at Embudo, NM
 startDate <- "1895-02-01"
 endDate <- Sys.Date()
 
@@ -30,13 +36,15 @@ gauge <- readNWISdv(siteNo, pCode, startDate, endDate,
            statCd)
 
 
-
+# convert it to a tibble
 gaugetb <- as_tibble(gauge)
 
-
-
+# create variable for the year and day of the year
 gaugetb$year <- year(gaugetb$Date)
 gaugetb$yday <- yday(gaugetb$Date)
+
+# this code is temporarily disabled. Sometimes displaying 2017 was screwing up the
+# joyplot, dunno why
 
 # remove 2017, not sure why this was screwing things up
 #gaugetb <- filter(gaugetb, year<2017)
