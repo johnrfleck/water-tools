aea19fb19f22bb9e119cde43705deb8540d036ee
johnrfleck <jfleck@inkstain.net>
2018-04-09 21:20:07 -0600

    Adding "gauge_this_year.r", which pulls data for a specified gauge and plots a year-to-date graph comparing current year with history.

---
+++ gauge_this_year.R
@@ -0,0 +1,55 @@
+# download general data on a stream gauge
+# tutorial here: https://owi.usgs.gov/R/dataRetrieval.html#1
+# Albuquerque gauge to use as example: 08330000
+
+library(dataRetrieval)
+library(tidyverse)
+library(lubridate)
+
+#get gauge number
+siteNo <- readline(prompt="Enter a gauge number: ")
+
+# get station metadata
+gauge_meta <- readNWISsite(siteNo)
+
+# reasonable start date
+startDate <- "1895-02-01"
+endDate <- Sys.Date()
+
+# statistics of interest - this is mean daily discharge
+pCode <- "00060"
+statCd <- "00003"
+
+# retrieve data
+gauge <- as_tibble(gauge <- readNWISdv(siteNo, pCode, startDate, endDate,
+                    statCd))
+# use pipes to rename flow variable, select ones we need, and
+# add the year
+gauge_daily <- gauge %>%
+  rename(flow = X_00060_00003) %>%
+  select(Date, flow) %>%
+  mutate(year=year(Date))
+
+#check to remove spurious negative number that showed up that one time
+gauge_daily <- filter(gauge_daily, flow>0)
+
+# get today's yday
+
+today <- yday(Sys.time())
+
+
+gauge_daily$yday <- yday(gauge_daily$Date)
+
+#filter up to one day after today's date
+
+gauge_daily <- filter(gauge_daily, yday < (today+1))
+
+this_year <- filter(gauge_daily, year==2018)
+
+ggplot(data=gauge_daily, aes(x=yday, y=flow+1, group=year)) + 
+  geom_path(alpha=0.1) +
+  ylab("cfs, daily average") +
+  xlab("day of the year") +
+  geom_path(data=this_year, aes(x=yday, y=flow+1, colour="blue"), show.legend=F, size=1) +
+  ggtitle(gauge_meta$station_nm)
+  
