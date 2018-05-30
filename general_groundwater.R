4dee48c190d9b69291082587ce5daf027d27cf00
johnrfleck <jfleck@inkstain.net>
2018-05-30 05:42:39 -0600

    intial commit of general_groundwater.r, which pulls and graphs groundwater levels

---
+++ general_groundwater.R
@@ -0,0 +1,46 @@
+# get, plot general groundwater data
+# tutorial here: https://owi.usgs.gov/R/dataRetrieval.html#1
+
+library(dataRetrieval)
+library(tidyverse)
+
+# parameters for USGS API
+
+#get gauge number
+
+siteNo <- readline(prompt="Enter a gauge number: ")
+
+# parameters for USGS API
+
+sta_meta <- readNWISsite(siteNo)
+
+print(sta_meta$station_nm)
+
+
+
+
+# reasonable start date
+startDate <- "1970-10-01"
+endDate <- Sys.Date()
+
+# statistics of interest - this is groundwater level
+pCode <- "72019"
+statCd <- "00002"
+
+# retrieve data
+gw <- as_tibble(readNWISdv(siteNo, pCode, startDate, endDate,
+           statCd))
+
+#create plot
+
+
+p <- ggplot(gw, aes(x=Date, y=(X_72019_00002*(-1)))) +
+  geom_line(alpha=0.3) +
+  stat_smooth(method="loess", se="false", span=0.2) +
+  ylab("depth below surface") +
+  ggtitle(sta_meta$station_nm)
+
+
+
+print(p)
+
