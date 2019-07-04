1cd4eabf94b72e44145ee170b292a00dae4bfba0
johnrfleck <jfleck@inkstain.net>
2019-07-04 16:10:45 -0600

    change graph from old cloud of past years' ghosts to color bands at the 10-90 and 30-70 percentiles. Add legend.

--- gauge_this_year.R
+++ gauge_this_year.R
@@ -44,25 +44,34 @@ gauge_daily$yday <- yday(gauge_daily$Date)
 startdate <- paste("Daily flows\nUSGS gauge ",
                    gauge_meta$site_no,
                    "\nData series start date: ",
-                   gauge_daily$Date[1], 
-                   "\nblue 2019, red 2018, green 2017, gray all previous years")
+                   gauge_daily$Date[1])
 
-# filter up to one day after today's date
-# gauge_daily <- filter(gauge_daily, yday < (today+1))
-
-last_year <- filter(gauge_daily, year==2018)
 this_year <- filter(gauge_daily, year==2019)
-seventeen <- filter(gauge_daily, year==2017)
 
-ggplot(data=gauge_daily, aes(x=yday, y=flow+1, group=year)) + 
-  geom_path(alpha=0.1) +
-  geom_path(data=seventeen, aes(x=yday, y=flow+1), colour="green", show.legend=F, size=1) +
-  geom_path(data=last_year, aes(x=yday, y=flow+1), colour="firebrick1", show.legend=F, size=1) +
-  geom_path(data=this_year, aes(x=yday, y=flow+1), colour="blue", show.legend=F, size=1) +
+
+by_day <- group_by(gauge_daily, yday)
+gauge_summary <- summarize(by_day, 
+                           mx = max(flow),
+                           mn = min(flow), 
+                           p90=quantile(flow, .9),
+                           p70=quantile(flow, .7),
+                           p30=quantile(flow, .3),
+                           p10=quantile(flow, .1),
+                           md=median(flow))
+ggplot(gauge_summary, aes(yday)) +
+  theme_bw() +
+  geom_ribbon(aes(ymin=p10, ymax=p90, fill="10th to 90th percentile")) +
+  geom_ribbon(aes(ymin=p30, ymax=p70, fill="30th to 70th percentile")) +
+  geom_path(aes(x=yday, y=md, color="median"), linetype = 2) +
+  geom_path(data=this_year, aes(x=yday, y=flow+1, color="2019")) +
+  scale_colour_manual(values=c("black","darkgrey"))+
+  scale_fill_manual(values=c("lightblue","lightgreen")) +
+  guides(fill=guide_legend(title=NULL)) +
+  guides(colour=guide_legend(title=NULL)) +
   labs(title=gauge_meta$station_nm,
        x = "day of the year",
        y = "flow, cubic feet per second",
        caption = "Data: USGS\ngraph: University of New Mexico Water Resources Program\ncode: https://github.com/johnrfleck/water-tools",
-       subtitle=startdate) +
+       subtitle=startdate)
   theme(strip.text = element_text(face = "bold", size = 8))
   
