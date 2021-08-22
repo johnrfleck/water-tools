19c21f2c4fa5e01a3b51aeebdd92965709ff35a3
John Fleck <johnfleck@Johns-iMac.local>
2021-08-22 16:36:10 -0600

    change threshold selection

--- flow_threshold.r
+++ flow_threshold.r
@@ -24,6 +24,11 @@ gauge_meta <- readNWISsite(siteNo)
 startDate <- "1895-02-01"
 endDate <- Sys.Date()
 
+startdate_text <- paste(gauge_meta$station_nm,", ", gauge_meta$site_no,
+                        "\nData series start date: ",
+                        gauge_daily$Date[1])
+
+
 # statistics of interest - this is mean daily discharge
 pCode <- "00060"
 statCd <- "00003"
@@ -35,7 +40,7 @@ gauge <- as_tibble(gauge <- readNWISdv(siteNo, pCode, startDate, endDate,
 # add the year
 gauge_daily <- gauge %>%
   rename(flow = X_00060_00003) %>%
-  select(Date, flow) %>%
+  select(Date, flow, site_no) %>%
   mutate(year=year(Date))
 
 #check to remove spurious negative number that showed up that one time
@@ -50,7 +55,7 @@ ggplot(data=tograph, aes(x=year, y=n)) +
   xlim(min(gauge_daily$year),max(gauge_daily$year)) +
   geom_bar(stat="identity") + 
   labs(title=paste("Days with flow", threshold, "cfs or less"),
-       subtitle=gauge_meta$station_nm, 
+       subtitle = startdate_text,
        x="year", 
        y="number of days", 
        caption="Data: USGS\ngraph: John Fleck, University of New Mexico Water Resources Program")
