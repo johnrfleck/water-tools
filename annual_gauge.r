0a06b441a24ac69024b969d1512cbd13adc71532
John Fleck <jfleck@inkstain.net>
2022-12-27 18:07:57 -0700

    Updating for 2022, graphics cleanup

--- annual_gauge.r
+++ annual_gauge.r
@@ -13,10 +13,7 @@ siteNo <- readline(prompt="Enter a gauge number: ")
 
 # get station metadata
 gauge_meta <- readNWISsite(siteNo)
-startdate <- paste("annual flow\nUSGS gauge",
-                   gauge_meta$site_no,
-                   "\nData series start date: ",
-                   gauge_daily$Date[1])
+
 
 # reasonable start date
 startDate <- "1895-02-01"
@@ -31,30 +28,34 @@ gauge <- as_tibble(gauge <- readNWISdv(siteNo, pCode, startDate, endDate,
                     statCd))
 # use pipes to rename flow variable, select ones we need, and
 # add the year
+startdate <- paste("annual flow\nUSGS gauge",
+                   gauge_meta$site_no,
+                   "\nData series start date: ",
+                   gauge_daily$Date[1])
 gauge_daily <- gauge %>%
   rename(flow = X_00060_00003) %>%
   select(Date, flow) %>%
   mutate(year=year(Date)) %>%
-  filter(year < 2021)
+  filter(year < 2022)
 
 # create annual average flows
 # the *724 part converts cfs to af per year
 gauge_annual <- summarize(group_by(gauge_daily, year), annual_flow=mean(flow)*724)
 
 # plot average annual flows
-p <- ggplot(gauge_annual, aes(year, annual_flow)) +
-  geom_line(data=gauge_annual, aes(x=year, y=mean(gauge_annual$annual_flow)), colour="brown", size=0.5) +
-  geom_line(data=gauge_annual, aes(x=year, y=median(gauge_annual$annual_flow)), colour="red", size=0.5) +
+p <- ggplot(gauge_annual, aes(year, annual_flow/1000)) +
+  geom_line(data=gauge_annual, aes(x=year, y=mean(gauge_annual$annual_flow/1000)), colour="brown", size=0.5) +
+  # geom_line(data=gauge_annual, aes(x=year, y=median(gauge_annual$annual_flow)), colour="red", size=0.5) +
   geom_bar(stat="identity") +
   ylab("acre feet") +
   xlab("Data: USGS; graph by John Fleck, University of New Mexico Water Resources Program") +
   ggtitle(gauge_meta$station_nm, subtitle="annual flow") +
-  annotate("text", x=2020, y=mean(gauge_annual$annual_flow), label="mean", size=3) +
-  annotate("text", x=2020, y=median(gauge_annual$annual_flow), label="median", size=3) +
+  annotate("text", x=2023, y=mean(gauge_annual$annual_flow/1000), label="mean", size=3) +
+  # annotate("text", x=2020, y=median(gauge_annual$annual_flow), label="median", size=3) +
   labs(title=gauge_meta$station_nm,
        subtitle=startdate,
        x = "year",
-       y = "acre feet",
+       y = "thousand acre feet",
        caption = "Data: USGS\ngraph: University of New Mexico Water Resources Program
        code: https://github.com/johnrfleck/water-tools") +
   theme(strip.text = element_text(face = "bold", size = 8))
