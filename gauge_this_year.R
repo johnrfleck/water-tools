7c324c16d023bb06299c6dc6703bfdc6febeb394
John Fleck <jfleck@inkstain.net>
2022-12-27 17:49:12 -0700

    Update code for 2022 Update institutional affiliation to Utton Center

--- gauge_this_year.R
+++ gauge_this_year.R
@@ -46,35 +46,39 @@ startdate <- paste("Daily flows\nUSGS gauge ",
                    "\nData series start date: ",
                    gauge_daily$Date[1])
 
-this_year <- filter(gauge_daily, year==2021)
-last_year <- filter(gauge_daily, year==2020)
+this_year <- filter(gauge_daily, year==2022)
+last_year <- filter(gauge_daily, year==2021)
 last_year$newflow <- last_year$flow
 
 by_day <- group_by(gauge_daily, yday)
 gauge_summary <- summarize(by_day, 
                            mx = max(flow, na.rm = TRUE),
                            mn = min(flow, na.rm = TRUE), 
+      
                            p90=quantile(flow, .9, na.rm = TRUE),
                            p70=quantile(flow, .7, na.rm = TRUE),
                            p30=quantile(flow, .3, na.rm = TRUE),
                            p10=quantile(flow, .1, na.rm = TRUE),
                            md=median(flow, na.rm = TRUE))
-ggplot(gauge_summary, aes(yday)) +
+p <- ggplot(gauge_summary, aes(yday)) +
   theme_bw() +
+  geom_ribbon(aes(ymin=mn, ymax=mx, fill="min to max"))+
   geom_ribbon(aes(ymin=p10, ymax=p90, fill="10th to 90th percentile")) +
   geom_ribbon(aes(ymin=p30, ymax=p70, fill="30th to 70th percentile")) +
   geom_path(aes(x=yday, y=md, color="median"), linetype = 2) +
-  geom_path(data=this_year, aes(x=yday, y=flow+1, color="2021")) +
-  geom_path(data=last_year, aes(x=yday, y=newflow+1, color="2020")) +
+  geom_path(data=this_year, aes(x=yday, y=flow+1, color="2022")) +
+  geom_path(data=last_year, aes(x=yday, y=newflow+1, color="2021")) +
   scale_colour_manual(values=c("black","red","darkgrey"))+
-  scale_fill_manual(values=c("lightblue","lightgreen")) +
+  scale_fill_manual(values=c("lightblue","lightgreen","lightgray")) +
   guides(fill=guide_legend(title=NULL)) +
   guides(colour=guide_legend(title=NULL)) +
-  #scale_y_log10() +
+  scale_y_log10() +
   labs(title=gauge_meta$station_nm,
        x = "day of the year",
        y = "flow, cubic feet per second",
-       caption = "Data: USGS\ngraph: University of New Mexico Water Resources Program\ncode: https://github.com/johnrfleck/water-tools",
+       caption = "Data: USGS\ngraph: Utton Center, University of New Mexico\ncode: https://github.com/johnrfleck/water-tools",
        subtitle=startdate)
   theme(strip.text = element_text(face = "bold", size = 8))
+  
+  print(p)
   
